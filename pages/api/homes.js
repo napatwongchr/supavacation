0c82be0ec4e114a753615d673200b6d154a35c1d
James <knotsite@gmail.com>
2022-11-26 22:19:48 +0700

    feat(home): create home with user id

--- pages/api/homes.js
+++ pages/api/homes.js
@@ -1,13 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/react";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   // Create new home
   if (req.method === "POST") {
     try {
       const { image, title, description, price, guests, beds, baths } =
         req.body;
+
+      const user = await prisma.user.findUnique({
+        where: { email: session.user.email },
+      });
 
       const home = await prisma.home.create({
         data: {
@@ -18,6 +29,7 @@ export default async function handler(req, res) {
           guests,
           beds,
           baths,
+          ownerId: user.id,
         },
       });
       res.status(200).json(home);
